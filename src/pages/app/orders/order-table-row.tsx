fd7c063080a138c27986b8815672f87c8164fd17
DenisRaineri <denispraineri@gmail.com>
2024-01-23 16:17:51 -0300

    feature-páginas, componentes, interface

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,49 @@
+import { ArrowRight, Search, X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableCell, TableRow } from "@/components/ui/table";
+
+import { OrderDetails } from "./order-details";
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="sm">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        8ads8sd8d8as8d5sadasd4as
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium"> Denis Raineri</TableCell>
+      <TableCell className="font-medium">R$ 149,00</TableCell>
+      <TableCell>
+        <Button variant="outline" size="sm">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="sm">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}
